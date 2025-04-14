5f2932fa8381c9c3f14cf23fb0e3f133b7b14da5
p-mackenzie <dev.anakarina94@gmail.com>
2025-04-13 22:25:10 -0300

    feat: setup inicial do projeto

---
+++ src/main/java/caetano/ana/CadastroDecolaTech/Application.java
@@ -0,0 +1,13 @@
+package caetano.ana.CadastroDecolaTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDecolaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDecolaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/caetano/ana/CadastroDecolaTech/CadastroDecolaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package caetano.ana.CadastroDecolaTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDecolaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
